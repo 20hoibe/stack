da1f62cefabb9c5924306c9299dd9664cb6d59f0
Philipp Renoth <renoth@wildoak.de>
2018-09-21 22:27:19 +0200

    register shortcut

--- public/electron.js
+++ public/electron.js
@@ -1,17 +1,22 @@
-const {app, ipcMain, BrowserWindow, Tray, Menu} = require('electron');
+const {app, ipcMain, BrowserWindow, Tray, Menu, globalShortcut} = require('electron');
 const path = require('path');
 const isDev = require('electron-is-dev');
 
 let mainWindow;
 
-function createWindow() {
+const createWindow = () => {
   mainWindow = new BrowserWindow({width: 900, height: 680});
   mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
   mainWindow.on('closed', () => mainWindow = null);
-}
+};
 
 app.on('ready', () => {
   createWindow();
+
+  globalShortcut.register('CommandOrControl+Shift+J', () => {
+    console.log('Command+Shift+Alt+J');
+    // make screenshot
+  });
 });
 
 app.on('window-all-closed', () => {
